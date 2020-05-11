255860abff64f9589976a840fadd2ae06d3b28fe
unknown <megalex97@hotmail.com>
2020-05-11 19:15:16 +0300

    test files pep8ed

--- website/tests/test_favorites.py
+++ website/tests/test_favorites.py
@@ -22,13 +22,16 @@ class FavoriteTest(TestCase):
         user = User.objects.get(username='testuser@mail.com')
         hospital = Hospital.objects.get(name='Spitalul Clinic CFR')
         self.client.force_login(user)
-        response = self.client.post(reverse('add_to_favorites'), {'hospital': hospital.name, 'specialty':'doesntexist'})
+        response = self.client.post(
+            reverse('add_to_favorites'),
+            {'hospital': hospital.name, 'specialty': 'doesntexist'})
         self.assertEqual(response.status_code, 500)
 
     def test_create_favorite_without_user_gives_error(self):
-        user = User.objects.get(username='testuser@mail.com')
         hospital = Hospital.objects.get(name='Spitalul Clinic CFR')
-        response = self.client.post(reverse('add_to_favorites'), {'hospital': hospital.name, 'specialty':'doesntexist'})
+        response = self.client.post(
+            reverse('add_to_favorites'),
+            {'hospital': hospital.name, 'specialty': 'doesntexist'})
         self.assertEqual(response.status_code, 500)
 
     def test_method_is_not_post_gives_error(self):
@@ -42,9 +45,17 @@ class FavoriteTest(TestCase):
         hospital = Hospital.objects.get(name='Spitalul Clinic CFR')
         specialty = Specialty.objects.get(name='Chirurgie Generală')
         self.client.force_login(user)
-        response = self.client.post(reverse('add_to_favorites'), {'hospital': hospital.name, 'specialty':specialty.name})
-        added_favorite = Favorite.objects.filter(user=user, service__hospital=hospital, service__specialty=specialty).first()
+        self.client.post(
+            reverse('add_to_favorites'),
+            {
+                'hospital': hospital.name,
+                'specialty': specialty.name})
+        added_favorite = Favorite.objects.filter(
+            user=user,
+            service__hospital=hospital,
+            service__specialty=specialty).first()
         self.assertEqual(added_favorite in favorites, True)
+
 
 class RemoveFromFavoriteTest(TestCase):
     fixtures = ["test_data", 'initial_data']
@@ -52,8 +63,9 @@ class RemoveFromFavoriteTest(TestCase):
     def test_bad_favorite_gives_error(self):
         favorite_id = 999
         with self.assertRaises(AttributeError):
-            self.client.post(reverse('remove_from_fav'), {'favorite_id': favorite_id})
-        
+            self.client.post(
+                reverse('remove_from_fav'),
+                {'favorite_id': favorite_id})
 
     def test_favorite_is_removed_with_success(self):
         user = User.objects.get(username='testuser@mail.com')
@@ -61,6 +73,8 @@ class RemoveFromFavoriteTest(TestCase):
         new_fav = Favorite(user=user, service=service)
         new_fav.save()
         favorite_id = new_fav.id
-        response = self.client.get(reverse('remove_from_fav'), {'favorite_id': favorite_id})
+        response = self.client.get(
+            reverse('remove_from_fav'),
+            {'favorite_id': favorite_id})
         self.assertEqual(len(Favorite.objects.filter(id=favorite_id)), 0)
         self.assertEqual(response.content.decode('utf-8'), 'Favorite deleted')
--- website/tests/test_paperwork.py
+++ website/tests/test_paperwork.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.urls import reverse
-from website.models import Candidate, Hospital, Service, Specialty, City, Favorite, Paperwork_Service, Paperwork, Paperwork_Service_User
+from website.models import Service
+from website.models import Paperwork_Service, Paperwork
+from website.models import Paperwork_Service_User
 import json
 from django.db.utils import IntegrityError
+
 
 class GetListofPaperWorkTest(TestCase):
     fixtures = ["test_data", 'initial_data']
@@ -12,31 +15,43 @@ class GetListofPaperWorkTest(TestCase):
         user = User.objects.get(username='testuser@mail.com')
         self.client.force_login(user)
         service_id = 999
-        response = self.client.get(reverse('get_list_of_paperwork'), {'user': user, 'service_id':service_id})
+        response = self.client.get(
+            reverse('get_list_of_paperwork'),
+            {'user': user, 'service_id': service_id})
         self.assertEqual(len(json.loads(response.content.decode('utf-8'))), 0)
-        
 
     def test_anonymous_user_return_error(self):
         user = User.objects.get(username='testuser@mail.com')
         service_id = 1
-        with self.assertRaises(TypeError): 
-            self.client.get(reverse('get_list_of_paperwork'), {'user': user, 'service_id':service_id})
-       
+        with self.assertRaises(TypeError):
+            self.client.get(
+                reverse('get_list_of_paperwork'),
+                {'user': user, 'service_id': service_id})
 
     def test_return_right_list_of_paperwork(self):
         user = User.objects.get(username='testuser@mail.com')
         service = Service.objects.all().first()
         paperwork = Paperwork.objects.all().first()
-        pw_service = Paperwork_Service.objects.get(service=service, paperwork=paperwork)
-        new_paperwork_service_user = Paperwork_Service_User(user=user, pw_service=pw_service)
+        pw_service = Paperwork_Service.objects.get(
+            service=service,
+            paperwork=paperwork)
+        new_paperwork_service_user = Paperwork_Service_User(
+            user=user,
+            pw_service=pw_service)
         new_paperwork_service_user.save()
-        paperwork_service_user  = Paperwork_Service_User.objects.filter(user=user, pw_service__service=service)
+        paperwork_service_user = Paperwork_Service_User.objects.filter(
+            user=user,
+            pw_service__service=service)
         list_of_paperwork = []
         for item in paperwork_service_user:
             list_of_paperwork.append(item.pw_service.paperwork.name)
         self.client.force_login(user)
-        response = self.client.get(reverse('get_list_of_paperwork'), {'user': user, 'service_id':service.id})
-        self.assertCountEqual(json.loads(response.content.decode('utf-8')), list_of_paperwork)
+        response = self.client.get(
+            reverse('get_list_of_paperwork'),
+            {'user': user, 'service_id': service.id})
+        self.assertCountEqual(
+            json.loads(response.content.decode('utf-8')),
+            list_of_paperwork)
 
 
 class AddToPaperWorksTest(TestCase):
@@ -47,29 +62,50 @@ class AddToPaperWorksTest(TestCase):
         self.client.force_login(user)
         service_id = 999
         paperwork_name = 'Identification Document'
-        with self.assertRaises(IntegrityError): 
-            response = self.client.get(reverse('add_to_paperworks'), {'user':user, 'paperwork': paperwork_name, 'service_id':service_id})
+        with self.assertRaises(IntegrityError):
+            self.client.get(
+                reverse('add_to_paperworks'),
+                {
+                    'user': user,
+                    'paperwork': paperwork_name,
+                    'service_id': service_id
+                    })
 
     def test_anonymous_user_return_error(self):
         user = User.objects.get(username='testuser@mail.com')
         service_id = 5
         paperwork_name = 'Identification Document'
-        with self.assertRaises(ValueError): 
-            response = self.client.get(reverse('add_to_paperworks'), {'user':user, 'paperwork': paperwork_name, 'service_id':service_id})
+        with self.assertRaises(ValueError):
+            self.client.get(
+                reverse('add_to_paperworks'),
+                {
+                    'user': user,
+                    'paperwork': paperwork_name,
+                    'service_id': service_id
+                    })
 
     def test_paperwork_is_added_with_success(self):
         user = User.objects.get(username='testuser@mail.com')
         self.client.force_login(user)
         service_id = 5
         paperwork_name = 'Identification Document'
-        pw_service = Paperwork_Service.objects.filter(service=service_id, paperwork__name=paperwork_name).first()
-        response = self.client.get(reverse('add_to_paperworks'), {'user':user, 'paperwork': paperwork_name, 'service_id':service_id})
-        documents = Paperwork_Service_User.objects.filter(user=user, pw_service=pw_service)
+        pw_service = Paperwork_Service.objects.filter(
+            service=service_id,
+            paperwork__name=paperwork_name).first()
+        self.client.get(
+            reverse('add_to_paperworks'),
+            {
+                'user': user,
+                'paperwork': paperwork_name,
+                'service_id': service_id})
+        documents = Paperwork_Service_User.objects.filter(
+            user=user,
+            pw_service=pw_service)
         paperwork_list = []
         for item in documents:
             paperwork_list.append(item.pw_service.paperwork.name)
         self.assertEqual(paperwork_name in paperwork_list, True)
-  
+
 
 class RemoveToPaperWorkTest(TestCase):
     fixtures = ["test_data", 'initial_data']
@@ -79,36 +115,65 @@ class RemoveToPaperWorkTest(TestCase):
         self.client.force_login(user)
         service_id = 5
         paperwork_name = 'Identification Document'
-        pw_service = Paperwork_Service.objects.filter(service=service_id, paperwork__name=paperwork_name).first()
-        self.client.get(reverse('add_to_paperworks'), {'user':user, 'paperwork': paperwork_name, 'service_id':service_id})
+        self.client.get(
+            reverse('add_to_paperworks'),
+            {
+                'user': user,
+                'paperwork': paperwork_name,
+                'service_id': service_id})
 
     def test_fake_service_return_error(self):
         user = User.objects.get(username='testuser@mail.com')
         self.client.force_login(user)
         service_id = 999
         paperwork_name = 'Identification Document'
-        pw_service = Paperwork_Service.objects.filter(service=service_id, paperwork__name=paperwork_name).first()
+        Paperwork_Service.objects.filter(
+            service=service_id,
+            paperwork__name=paperwork_name).first()
         with self.assertRaises(AttributeError):
-            self.client.get(reverse('remove_to_paperworks'), {'user':user, 'paperwork': paperwork_name, 'service_id':service_id})
+            self.client.get(
+                reverse('remove_to_paperworks'),
+                {
+                    'user': user,
+                    'paperwork': paperwork_name,
+                    'service_id': service_id
+                    })
 
     def test_anonymous_user_return_error(self):
         user = User.objects.get(username='testuser@mail.com')
         self.client.logout()
         service_id = 5
         paperwork_name = 'Identification Document'
-        pw_service = Paperwork_Service.objects.filter(service=service_id, paperwork__name=paperwork_name).first()
+        Paperwork_Service.objects.filter(
+            service=service_id,
+            paperwork__name=paperwork_name).first()
         with self.assertRaises(TypeError):
-            self.client.get(reverse('remove_to_paperworks'), {'user':user, 'paperwork': paperwork_name, 'service_id':service_id})
-        
+            self.client.get(
+                reverse('remove_to_paperworks'),
+                {
+                    'user': user,
+                    'paperwork': paperwork_name,
+                    'service_id': service_id
+                    })
 
     def test_paperwork_is_removed_with_success(self):
         user = User.objects.get(username='testuser@mail.com')
         self.client.force_login(user)
         service_id = 5
         paperwork_name = 'Identification Document'
-        pw_service = Paperwork_Service.objects.filter(service=service_id, paperwork__name=paperwork_name).first()
-        self.client.get(reverse('remove_to_paperworks'), {'user':user, 'paperwork': paperwork_name, 'service_id':service_id})
-        documents = Paperwork_Service_User.objects.filter(user=user, pw_service=pw_service)
+        pw_service = Paperwork_Service.objects.filter(
+            service=service_id,
+            paperwork__name=paperwork_name).first()
+        self.client.get(
+            reverse('remove_to_paperworks'),
+            {
+                'user': user,
+                'paperwork': paperwork_name,
+                'service_id': service_id
+                })
+        documents = Paperwork_Service_User.objects.filter(
+            user=user,
+            pw_service=pw_service)
         paperwork_list = []
         for item in documents:
             paperwork_list.append(item.pw_service.paperwork.name)
