0d7c9fb39b4fd21a7da063a30a487f5b9c2ae974
unknown <megalex97@hotmail.com>
2020-05-12 15:28:01 +0300

    added static root

--- resio/settings.py
+++ resio/settings.py
@@ -139,5 +139,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 ALLOWED_HOSTS = ['resio-ro.herokuapp.com', '127.0.0.1:8000']
